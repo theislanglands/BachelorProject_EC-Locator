464aebed38b9a957d6dbcebe044e2821ee8fadc5
Theis Langlands <thlan20@student.sdu.dk>
2023-02-22 17:40:17 +0100

    prototype for teams repo

---
+++ EC-locator.Core/Settings.cs
@@ -0,0 +1,32 @@
+namespace EClocator.Core
+{
+    
+}
+
+// singleton holding Azure settings
+public sealed class Settings
+{
+    public string? ClientId { get; set; }
+    public string? ClientSecret { get; set; }
+    public string? TenantId { get; set; }
+    
+    // Singleton instance
+    private static Settings _instance;
+    
+    public static Settings GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Settings();
+        }
+        
+        return _instance;
+    }
+
+    public void initForAppAuth(string ClientId, string ClientSecret, string TenantId)
+    {
+        this.ClientId = ClientId;
+        this.ClientSecret = ClientSecret;
+        this.TenantId = TenantId;
+    }
+}
