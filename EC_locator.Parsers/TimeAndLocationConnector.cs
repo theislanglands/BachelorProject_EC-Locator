7b5f9968ed87de43faa8e3642498f41f50f0bd50
Theis Langlands <thlan20@student.sdu.dk>
2023-04-18 22:22:36 +0200

    refactor TimeAndLocationConnector

--- EC_locator.Parsers/TimeAndLocationConnector.cs
+++ EC_locator.Parsers/TimeAndLocationConnector.cs
@@ -10,15 +10,16 @@ public class TimeAndLocationConnector : ITimeAndLocationConnector
     private readonly bool _verbose;
     private readonly TimeOnly _workStartDefault;
     private readonly TimeOnly _workEndDefault;
-    
+
     private SortedList<int, Location> _locationTags;
     private SortedList<int, TimeOnly> _timeTags;
 
-    public TimeAndLocationConnector(IOptions<VerboseOptions> settingsOptions, IOptions<DefaultLocationOptions> locationOptions)
+    public TimeAndLocationConnector(IOptions<VerboseOptions> settingsOptions,
+        IOptions<DefaultLocationOptions> locationOptions)
     {
         string[] workStart = locationOptions.Value.DefaultWorkStart.Split(':');
         string[] workEnd = locationOptions.Value.DefaultWorkEnd.Split(':');
-        _workStartDefault = new TimeOnly(int.Parse(workStart[0]),int.Parse(workStart[1]));
+        _workStartDefault = new TimeOnly(int.Parse(workStart[0]), int.Parse(workStart[1]));
         _workEndDefault = new TimeOnly(int.Parse(workEnd[0]), int.Parse(workEnd[1]));
         _verbose = settingsOptions.Value.Verbose;
     }
@@ -28,12 +29,12 @@ public class TimeAndLocationConnector : ITimeAndLocationConnector
         _locationTags = locationTags;
         _timeTags = timeTags;
         var locationsFound = new List<Location>();
-        
+
         if (_verbose)
         {
             Console.WriteLine("Adding times to locations");
         }
-        
+
         // adding times to location tags
         for (int locationIndex = 0; locationIndex < _locationTags.Count; locationIndex++)
         {
@@ -44,35 +45,30 @@ public class TimeAndLocationConnector : ITimeAndLocationConnector
 
         return locationsFound;
     }
-    
+
     private void SetStartTime(int locationIndex)
     {
-        // if first location use default work start
+        // if first location use default work start otherwise end time of previous locations
         if (locationIndex == 0)
         {
             _locationTags.Values[locationIndex].Start = _workStartDefault;
         }
-        // if not first location, set start time to end time of previous location
         else
         {
-            _locationTags.Values[locationIndex].Start = _locationTags.Values[locationIndex-1].End;
+            _locationTags.Values[locationIndex].Start = _locationTags.Values[locationIndex - 1].End;
         }
     }
-    
+
     private void SetEndTime(int locationIndex)
     {
-        // run if end time has not been set
-        if (_locationTags.Values[locationIndex].End == null)
+        // set to endtime to start of next locations, if last the default
+        if (locationIndex == _locationTags.Count - 1)
         {
-            // if last location has and end-indicator, set to last time, otherwise default
-            if (locationIndex == _locationTags.Count - 1)
-            {
-                _locationTags.Values[locationIndex].End = _workEndDefault;
-            }
-            else
-            {
-                _locationTags.Values[locationIndex].End = _timeTags.Values[locationIndex];
-            }
+            _locationTags.Values[locationIndex].End = _workEndDefault;
+        }
+        else
+        {
+            _locationTags.Values[locationIndex].End = _timeTags.Values[locationIndex];
         }
     }
 }
