c6a6a21f68ed37ceed9d00459e8efed19496f941
Theis Langlands <thlan20@student.sdu.dk>
2023-04-20 14:33:46 +0200

    fetching calendar info in LocationController

--- EC-locator.API/Controllers/LocationController.cs
+++ EC-locator.API/Controllers/LocationController.cs
@@ -1,15 +1,15 @@
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using EC_locator.Core;
 using EC_locator.Core.Interfaces;
 using EC_locator.Core.Models;
+using Location = EC_locator.Core.Models;
+using Message = EC_locator.Core.Models;
 using EC_locator.Core.SettingsOptions;
-using EC_locator.Repositories;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using EC_locator.Parsers;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+
 
 namespace API.Controllers;
 
@@ -38,8 +38,10 @@ public class LocationController
     [HttpGet("{employeeId}")]
     public async Task<string> GetCurrentLocationAsync(string employeeId)
     {
-        Location? currentLocation = _employeeLocator.GetCurrentLocation(employeeId);
-        Message? latestMessage = _employeeLocator.GetLatestMessage(employeeId);
+        Location.Location? currentLocation = _employeeLocator.GetCurrentLocation(employeeId);
+        Location.Message? latestMessage = _employeeLocator.GetLatestMessage(employeeId);
+        List<CalendarEvent>? calendarEvents = _employeeLocator.GetCurrentCalendarEvents(employeeId);
+        
         LocationReturn lr = new LocationReturn();
         
         if (_verbose)
@@ -48,6 +50,11 @@ public class LocationController
             Console.WriteLine($"EmployeeID: {employeeId}");
             Console.WriteLine($"Latest Message: {latestMessage}");
             Console.WriteLine($"Current Location: {currentLocation}");
+            if (calendarEvents != null)
+                foreach (var calendarEvent in calendarEvents)
+                {
+                    Console.WriteLine($"Calendar Event: {calendarEvent}");
+                }
         }
         
         if (latestMessage == null)
@@ -68,6 +75,18 @@ public class LocationController
             lr.TeamMessage = latestMessage.Content;
         }
 
+        if (calendarEvents != null)
+        {
+            lr.CurrentCalendarEvents = calendarEvents;
+            StringBuilder calendarInfo = new StringBuilder();
+            foreach (var calendarEvent in calendarEvents)
+            {
+                calendarInfo.Append(calendarEvent);
+            }
+
+            lr.CalenderInfo = calendarInfo.ToString();
+        }
+
         return JsonSerializer.Serialize(lr, _jsonOptions);
     }
 
@@ -80,5 +99,6 @@ public class LocationController
 
         public string TeamMessage { get; set; }
         public string CalenderInfo { get; set; }
+        public List<CalendarEvent> CurrentCalendarEvents { get; set; }
     }
 }
